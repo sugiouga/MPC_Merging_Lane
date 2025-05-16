5ed8ba6f6b1dc063a3c9f57ddaa449e56fdc7cf0
sugiouga <sugiouga@gmail.com>
2025-05-16 16:31:45 +0900

    no message

--- config.m
+++ config.m
@@ -2,6 +2,8 @@ TIME_STEP = 0.1; % タイムステップ (s)
 VIDEO_UPDATE_INTERVAL = 0.5; % 動画更新間隔 (s)
 INPUT_UPDATE_INTERVAL = 0.1; % 制御入力両更新間隔 (s)
 
+REACTION_TIME = 1.0; % 車両の反応時間 (s)
+BRAKE_COORDINATION = 0.2; % 車両のブレーキコーディネーション (s)
 PREDICTION_HORIZON = 40; % 予測ホライズン
 MERGE_POSITION = 400;
 
